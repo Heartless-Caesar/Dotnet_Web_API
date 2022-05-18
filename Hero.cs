db59fe9c678b1fb5e1c5771ce96e5420366c33c3
caesar <joaovictorcsatu@gmail.com>
2022-05-18 08:11:16 -0300

    Initial commit

---
+++ Hero.cs
@@ -0,0 +1,11 @@
+namespace MyWebAPI;
+
+public class Hero
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string Power { get; set; } = string.Empty;
+}
