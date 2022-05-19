2b610a09feeb1c3ab4e48e77a99f21d1a27d8434
caesar <joaovictorcsatu@gmail.com>
2022-05-19 09:41:22 -0300

    Added DB connection

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Models;
 
 namespace MyWebAPI.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){} 
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    public DbSet<Hero> Heroes { get; set; }
 }
